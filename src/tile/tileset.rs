907fb4b283f7713ae9635bc55de4f9ced6725885
Nathanael Ness <spiphy@gmail.com>
2024-03-11 19:23:12 -0700

    broken

--- src/tile/tileset.rs
+++ src/tile/tileset.rs
@@ -8,32 +8,13 @@ use super::tiledata::TileData;
 
 #[derive(Debug)]
 pub struct TileSet {
-    pub data: Vec<TileData>,
-    dimentions: [(i32,i32); 128]
+    pub data: Vec<TileData>
 }
 
 impl TileSet {
-    fn new() -> Self {
+    pub fn new() -> Self {
         TileSet {
-            data: Vec::new(),
-            dimentions: [
-                // 1      2      3      4      5      6      7      8      9     10     11     12     13     14     15     16
-                (1,1), (1,2), (1,3), (2,2), (3,2), (3,2), (2,4), (2,4), (3,3), (2,5), (4,3), (4,3), (3,5), (3,5), (3,5), (4,4),
-                // 17     18    19     20     21     22     23     24     25     26     27     28     29     30     31     32
-                (3,6), (3,6), (4,5), (4,5), (3,7), (4,6), (4,6), (4,6), (5,5), (4,7), (4,7), (4,7), (5,6), (5,6), (4,8), (4,8),
-                // 33     34    35     36     37     38     39     40     41     42     43     44     45     46     47     48
-                (6,6), (6,6), (6,6), (6,6), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
-                // 49     50    51     52     53     54     55     56     57     58     59     60     61     62     63     64
-                (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
-                // 65     66    67     68     69     70     71     72     73     74     75     76     77     78     79     80
-                (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
-                // 81     82    83     84     85     86     87     88     89     90     91     92     93     94     95     96
-                (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
-                // 97     98    99    100    101    102    103    104    105    106    107    108    109    110    111    112
-                (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
-                //113    114   115    116    117    118    119    120    121    122    123    124    125    126    127    128
-                (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0)
-            ]
+            data: Vec::new()
         }
     }
     pub fn read_file(&mut self, file_path: &str) -> io::Result<()> {
@@ -66,5 +47,32 @@ impl TileSet {
             }
         }
         Ok(())
+    }
+
+    pub fn write_bmp(&self, filepath: &str) -> io::Result<()> {
+        let mut f = OpenOptions::new().write(true).create(true).open(filepath)?;
+        f.write("BM".as_bytes());
+        f.write(0); // file size
+        f.write(0.as_bytes()); // unused
+        f.write(0.as_bytes()); // data offset
+        f.write(40.as_bytes()); // header size
+        f.write(1.as_buffer()); // width
+        f.write(1.as_bytes()); // height
+        f.write(1_u16.as_bytes()); // layers
+        f.write(4_u16.as_bytes()); // bits per pixel
+        f.write(0.as_bytes()); // compression
+        f.write(0.as_bytes()); // image data size
+        f.write(1.as_buffer()); // width pixels/meter
+        f.write(1.as_buffer()); // height pixels/meter
+        f.write(16.as_buffer()); // colors used
+        f.write(0.as_buffer()); // important colors
+        f.write(64.as_buffer()); // color table size
+        f.write(4294967040.as_bytes()); // white
+        f.write(2863311360.as_bytes()); // light grey
+        f.write(1431655680.as_bytes()); // dark grey
+        f.write(0.as_bytes()); // black
+        // image data
+        f.write(0.as_bytes());
+        OK(())
     }
 }
