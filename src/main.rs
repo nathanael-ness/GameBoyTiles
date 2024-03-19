e698926268c35f2b3a1a6ac58e7af6a1afd93168
Nathanael Ness <spiphy@gmail.com>
2024-03-19 05:17:03 -0700

    layout

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use tile::tileset::TileSet;
 
 mod tile;
 
-
-slint::slint!{
-    component MemoryTile inherits Rectangle {
+slint::slint! {
+    import { Button, GridBox } from "std-widgets.slint";
+component MemoryTile inherits Rectangle {
         width: 64px;
         height: 64px;
         background: #3960D5;
-    
+
         Image {
             source: @image-url("temp/image.png");
             width: parent.width;
@@ -19,12 +19,106 @@ slint::slint!{
     }
 
     export component MainWindow inherits Window {
-        width: 500px;
-        height: 500px;
-        MemoryTile {}
+        min-height: 600px;
+        min-width: 600px;
+        HorizontalLayout {
+            alignment: LayoutAlignment.start;
+            spacing: 20px;
+            padding: 20px;
+            VerticalLayout {
+                padding: 15px;
+                Button {text: "white";}
+                Button {text: "light grey";}
+                Button {text: "daek grey";}
+                Button {text: "black";}
+            }
+            GridLayout {
+                Row {
+                    Button {text: "Test0";}
+                    Button {text: "Test1";}
+                    Button {text: "Test2";}
+                    Button {text: "Test3";}
+                    Button {text: "Test4";}
+                    Button {text: "Test5";}
+                    Button {text: "Test6";}
+                    Button {text: "Test7";}
+                }
+                Row {
+                    Button {text: "Test0";}
+                    Button {text: "Test1";}
+                    Button {text: "Test2";}
+                    Button {text: "Test3";}
+                    Button {text: "Test4";}
+                    Button {text: "Test5";}
+                    Button {text: "Test6";}
+                    Button {text: "Test7";}
+                }
+                Row {
+                    Button {text: "Test0";}
+                    Button {text: "Test1";}
+                    Button {text: "Test2";}
+                    Button {text: "Test3";}
+                    Button {text: "Test4";}
+                    Button {text: "Test5";}
+                    Button {text: "Test6";}
+                    Button {text: "Test7";}
+                }
+                Row {
+                    Button {text: "Test0";}
+                    Button {text: "Test1";}
+                    Button {text: "Test2";}
+                    Button {text: "Test3";}
+                    Button {text: "Test4";}
+                    Button {text: "Test5";}
+                    Button {text: "Test6";}
+                    Button {text: "Test7";}
+                }
+                Row {
+                    Button {text: "Test0";}
+                    Button {text: "Test1";}
+                    Button {text: "Test2";}
+                    Button {text: "Test3";}
+                    Button {text: "Test4";}
+                    Button {text: "Test5";}
+                    Button {text: "Test6";}
+                    Button {text: "Test7";}
+                }
+                Row {
+                    Button {text: "Test0";}
+                    Button {text: "Test1";}
+                    Button {text: "Test2";}
+                    Button {text: "Test3";}
+                    Button {text: "Test4";}
+                    Button {text: "Test5";}
+                    Button {text: "Test6";}
+                    Button {text: "Test7";}
+                }
+                Row {
+                    Button {text: "Test0";}
+                    Button {text: "Test1";}
+                    Button {text: "Test2";}
+                    Button {text: "Test3";}
+                    Button {text: "Test4";}
+                    Button {text: "Test5";}
+                    Button {text: "Test6";}
+                    Button {text: "Test7";}
+                }
+                Row {
+                Button {text: "Test0";}
+                Button {text: "Test1";}
+                Button {text: "Test2";}
+                Button {text: "Test3";}
+                Button {text: "Test4";}
+                Button {text: "Test5";}
+                Button {text: "Test6";}
+                Button {text: "Test7";}
+            }
+        }
+        
+        }
+        // MemoryTile {}
     }
 }
-
 
 fn main() {
     MainWindow::new().unwrap().run().unwrap();
@@ -36,24 +130,24 @@ fn main() {
     // td.set_pixel(2, 2, 3);
     // td.set_pixel(3, 2, 3);
     // td.set_pixel(4, 2, 3);
-    // let px = td.get_pixel(2,2);    
+    // let px = td.get_pixel(2,2);
     // println!("{px}");
 
     //"/workspaces/GBT/snake.2bpp"
     let mut ts: TileSet = TileSet::new();
     let file_path = "snake.2bpp";
     let result = ts.read_file(file_path);
-    
+
     match result {
         Ok(()) => println!("File loaded"),
-        Err(error) => panic!("File not found {error}")
+        Err(error) => panic!("File not found {error}"),
     }
-    
+
     let result = ts.write_file("snake.test");
-    
+
     match result {
         Ok(()) => println!("File saved"),
-        Err(error) => panic!("File not saved {error}")
+        Err(error) => panic!("File not saved {error}"),
     }
 
     //let _ = ts.write_bmp();
